a426d51957b23252ea56b66dce19488d761fba5d
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-03-17 13:46:03 +0000

    Add comment to html request

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from bs4 import BeautifulSoup
 TARGET_URL = "https://www.amazon.co.uk/Automate-Boring-Stuff-Python-2nd/dp/1593279922/ref=sr_1_9?crid=G12YA8H6XOZ3" \
              "&keywords=python&qid=1679060293&sprefix=python%2Caps%2C67&sr=8-9"
 
+# HTML request
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0",
     "Accept-Language": "en-GB,en;q=0.5",
 }
-
 response = requests.get(TARGET_URL, headers=headers)
 response.encoding = "utf-8"
 html = response.text
