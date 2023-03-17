c7cfabbef6f3b1be00b4c63cfa5f4602c910ffe6
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-03-17 13:55:12 +0000

    Add beautifulsoup html parse

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 TARGET_URL = "https://www.amazon.co.uk/Automate-Boring-Stuff-Python-2nd/dp/1593279922/ref=sr_1_9?crid=G12YA8H6XOZ3" \
              "&keywords=python&qid=1679060293&sprefix=python%2Caps%2C67&sr=8-9"
+PRICE_THRESHOLD = 25.00
 
 # HTML request
 headers = {
@@ -13,4 +14,9 @@ headers = {
 response = requests.get(TARGET_URL, headers=headers)
 response.encoding = "utf-8"
 html = response.text
+
+# Scrape html data with BeautifulSoup and get current item price
+soup = BeautifulSoup(html, "html.parser")
+current_price = soup.find(name="span", class_="a-size-base a-color-price a-color-price").getText()
+current_price = float(current_price.replace("£", ""))
 
